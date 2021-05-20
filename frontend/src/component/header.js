f35ade1a58c57dc6fc388cab58ebf451bd24a36d
ayangd <jimmyhendrixx90@gmail.com>
2021-05-20 23:45:33 +0700

    Create menu on header

--- frontend/src/component/header.js
+++ frontend/src/component/header.js
@@ -1,20 +1,37 @@
-import { css, StyleSheet } from "aphrodite";
+import { createUseStyles } from "react-jss";
 
-const styles = StyleSheet.create({
+const useStyles = createUseStyles({
     header: {
         backgroundColor: '#5c99fa',
-    },
-    headerText: {
-        fontSize: '2em',
         padding: '8px 24px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    headerTitle: {
+        fontSize: '2em',
         margin: '0',
+    },
+    headerMenu: {
+        display: 'flex',
+        '& > li': {
+            listStyle: 'none',
+            marginLeft: '16px',
+        },
     },
 });
 
 function Header() {
+    const classes = useStyles();
+
     return (
-        <div className={css(styles.header)}>
-            <h4 className={css(styles.headerText)}>DesArt</h4>
+        <div className={classes.header}>
+            <h4 className={classes.headerTitle}>DesArt</h4>
+            <ul className={classes.headerMenu}>
+                <li>Menu 1</li>
+                <li>Menu 2</li>
+                <li>Menu 3</li>
+            </ul>
         </div>
     );
 }
